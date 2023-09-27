98752ba92e4903104aa416796144c083be34c705
LyesFrozenHeart <torresbravoalex@gmail.com>
2023-09-27 21:01:23 +0200

    Added enemy class so the UML is completed

---
+++ src/main/java/com/example/jocconversacionalalien/classes/Enemy.java
@@ -0,0 +1,11 @@
+package com.example.jocconversacionalalien.classes;
+
+public class Enemy extends Character{
+    private boolean asleep = true;
+    private int shiftCount = 0;
+    @Override
+    protected void ToTake() {}
+    @Override
+    protected void GoTo() {}
+    private void pairShift(int shiftCount){}
+}
