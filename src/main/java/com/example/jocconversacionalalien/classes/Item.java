b0ffa13e07fb95454dfbafe5465256e68ce9c46d
SergiAndreuVila <sergiandreuvila@gmail.com>
2023-09-27 20:51:57 +0200

    new classes
    added classes for game, item and zone

---
+++ src/main/java/com/example/jocconversacionalalien/classes/Item.java
@@ -0,0 +1,11 @@
+package com.example.jocconversacionalalien.classes;
+
+public class Item {
+
+  private String name;
+  private String description;
+  private boolean status;
+  private String localization;
+
+  private void useItem() {}
+}
