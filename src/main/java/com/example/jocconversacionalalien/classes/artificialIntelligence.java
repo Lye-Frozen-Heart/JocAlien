9abfd59f7f7f9253070e22e38d43c644951bdc61
LyesFrozenHeart <torresbravoalex@gmail.com>
2023-09-27 20:13:58 +0200

    Added articialIntelligence class and methods

---
+++ src/main/java/com/example/jocconversacionalalien/classes/artificialIntelligence.java
@@ -0,0 +1,20 @@
+package com.example.jocconversacionalalien.classes;
+
+public class artificialIntelligence {
+    private boolean angry = false;
+    private int randomQuestion = ((int) (Math.random()*(2 - 1))) + 1;
+
+    private void sayTruth(){
+
+    }
+    private void sayALie(){
+
+    }
+    private void openDoors(){
+
+    }
+
+    private void makeLifeImpossible(){
+
+    }
+}
