ff06c0d412e96e1f93f6b354a432413c41b4cef4
LyesFrozenHeart <torresbravoalex@gmail.com>
2023-09-28 21:02:32 +0200

    Added getters and setters for Zone.java properties and also added a new variable for better control of the exites of each one. Added the menu at the game with text-decorations...

--- src/main/java/com/example/jocconversacionalalien/classes/Game.java
+++ src/main/java/com/example/jocconversacionalalien/classes/Game.java
@@ -6,7 +6,7 @@ public class Game {
   private int countTurn;
   Zone[] zones = {};
   private static void menu() {
-     final int timeToWrite = 250;
+     final int timeToWrite = 120;
    Scanner scanner = new Scanner(System.in);
    WordElapser.writeWordWithTimeDelay(
 "Good to see you again Captain, have you slept well? Select a menu option:",timeToWrite);
@@ -23,7 +23,7 @@ public class Game {
        break;
      case 2:
        ColorChanger.printTextToGreen("/--//--//--//--//--//--//--//--//--/");
-       WordElapser.writeWordWithTimeDelay("This game has been developed by Alexandre Torres Bravo and Sergi Andreu Vila",200);
+       WordElapser.writeWordWithTimeDelay("This game has been developed by Alexandre Torres Bravo and Sergi Andreu Vila",timeToWrite);
        ColorChanger.printTextToGreen("/--//--//--//--//--//--//--//--//--/");
        ColorChanger.printTextToBlack("© All rights to be reserved...");
        break;
@@ -45,6 +45,51 @@ public class Game {
   }
   private static void exitGame() {
     System.exit(0);
+  }
+  private static Zone[] setUpZones(){
+    Zone[] zonesToReturn = null;
+    Zone workshop = new Zone();workshop.setIdZone(1);
+    //Parámetros del taller
+    workshop.setHasItem(false);workshop.setHasNpc(false);workshop.setHasLight(true);
+    workshop.setDoors(new boolean[]{false, false, true, false});workshop.setDirections(new int[]{-1,-1,1,-1});
+    workshop.setAccessible(true);workshop.setHasTraps(false);
+    //
+    Zone offices = new Zone();offices.setIdZone(2);
+    //Parámetros de las oficinas
+
+    //
+    Zone machineRoom = new Zone(); machineRoom.setIdZone(3);
+    //Parámetros de la sala de máquinas
+
+    //
+    Zone lockerRoom = new Zone(); lockerRoom.setIdZone(4);
+    //Parámetros del vestuario
+
+    //
+    Zone kitchen = new Zone(); kitchen.setIdZone(5);
+    //Parámetros de la cocina
+
+    //
+    Zone dinningRoom = new Zone(); dinningRoom.setIdZone(6);
+    //Parámetros del comedor
+
+    //
+    Zone bedRoom = new Zone(); bedRoom.setIdZone(7);
+    //Params del dormitorio
+
+    //
+    Zone bathRooms = new Zone(); bathRooms.setIdZone(8);
+    //Params de los Baños...
+
+    //
+    Zone exitRoom = new Zone(); exitRoom.setIdZone(9);
+    exitRoom.setHasItem(false);exitRoom.setHasNpc(false);exitRoom.setHasLight(true);
+    exitRoom.setDoors(new boolean[]{true, false, false, false});exitRoom.setDirections(new int[]{1,-1,-1,-1});
+    exitRoom.setAccessible(false);exitRoom.setHasTraps(false);
+    //Params de la salida
+    //Zone propulsors/Space = new Zone(); propulsors.setIdZone(10); TODO No hay décima zona únicamente hay 9 en todo el mapa!
+    zonesToReturn = new Zone[]{workshop, offices, machineRoom, lockerRoom, kitchen, dinningRoom, bedRoom, bathRooms, exitRoom};
+      return zonesToReturn;
   }
   public static void main(String[] args){
     menu();
--- src/main/java/com/example/jocconversacionalalien/classes/Zone.java
+++ src/main/java/com/example/jocconversacionalalien/classes/Zone.java
@@ -18,4 +18,76 @@ public class Zone {
   private void killPlayer() {}
 
   protected void useItem() {}
+
+  public int getIdZone() {
+    return idZone;
+  }
+
+  public void setIdZone(int idZone) {
+    this.idZone = idZone;
+  }
+
+  public boolean isHasItem() {
+    return hasItem;
+  }
+
+  public void setHasItem(boolean hasItem) {
+    this.hasItem = hasItem;
+  }
+
+  public Item[] getItems() {
+    return items;
+  }
+
+  public void setItems(Item[] items) {
+    this.items = items;
+  }
+
+  public boolean isHasNpc() {
+    return hasNpc;
+  }
+
+  public void setHasNpc(boolean hasNpc) {
+    this.hasNpc = hasNpc;
+  }
+
+  public boolean isHasLight() {
+    return hasLight;
+  }
+
+  public void setHasLight(boolean hasLight) {
+    this.hasLight = hasLight;
+  }
+
+  public boolean[] getDoors() {
+    return doors;
+  }
+
+  public void setDoors(boolean[] doors) {
+    this.doors = doors;
+  }
+
+  public int[] getDirections() {
+    return directions;
+  }
+
+  public void setDirections(int[] directions) {
+    this.directions = directions;
+  }
+
+  public boolean isAccessible() {
+    return accessible;
+  }
+
+  public void setAccessible(boolean accessible) {
+    this.accessible = accessible;
+  }
+
+  public boolean isHasTraps() {
+    return hasTraps;
+  }
+
+  public void setHasTraps(boolean hasTraps) {
+    this.hasTraps = hasTraps;
+  }
 }
